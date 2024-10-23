f5df95e2a3a0454a51921959845ea7ed75998638
Isis Yume Okamoto <isis.yume2508@gmail.com>
2024-10-23 19:56:17 -0300

    Atributos e métodos base das classes

--- src/main/java/com/example/trabalho/model/Aluno.java
+++ src/main/java/com/example/trabalho/model/Aluno.java
@@ -1,4 +1,38 @@
 package com.example.trabalho.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String nome;
+    private String RA;
+
+    public Aluno() {
+
+    }
+
+    public Aluno(String nome, String RA) {
+        this.nome = nome;
+        this.RA = RA;
+    }
+
+    public void fazerLogin() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRA() {
+        return RA;
+    }
+    public void setRA(String RA) {
+        this.RA = RA;
+    }
 }
--- src/main/java/com/example/trabalho/model/Solicitacao.java
+++ src/main/java/com/example/trabalho/model/Solicitacao.java
@@ -1,4 +1,64 @@
 package com.example.trabalho.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+
+@Entity
 public class Solicitacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String titulo;
+    private String assunto;
+    private String[] anexo;
+    private Date dataEnvio;
+
+    public Solicitacao() {
+
+
+    }
+
+    public void validarSolicitacao(){
+
+   }
+
+    public void visualizarSolicitacao(){
+
+   }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAssunto() {
+        return assunto;
+    }
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+    public String[] getAnexo() {
+        return anexo;
+    }
+    public void setAnexo(String[] anexo) {
+        this.anexo = anexo;
+    }
+    public Date getDataEnvio() {
+        return dataEnvio;
+    }
+    public void setDataEnvio(Date dataEnvio) {
+        this.dataEnvio = dataEnvio;
+    }
+
 }
