c8d211bf54cf0f36f8510239f3f61c1ff322083c
Isis Yume Okamoto <isis.yume2508@gmail.com>
2024-10-23 19:05:54 -0300

    teste

--- src/main/java/com/example/trabalho/controller/Teste.java
+++ src/main/java/com/example/trabalho/controller/Teste.java
@@ -2,10 +2,9 @@ package com.example.trabalho.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Hello {
+public class Teste {
     @GetMapping("/index")
     public String hello() {
         return "index.html";
