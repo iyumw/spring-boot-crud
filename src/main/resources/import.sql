9aceed9986a86ac6381d6100fad748e6656d2d7f
Amanda <amandavictoria11@outlook.com>
2024-11-04 16:39:54 -0300

    pesquisa funciona

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into funcionario(id,nome) values ('987654','danielle silva pereira', null);
-insert into funcionario(id,nome) values ( '652345','isis yume okamoto', null);
-insert into funcionario(id,nome) values ('459863', 'amanda victoria ribeiro', null);
+INSERT INTO funcionario(nome) VALUES ('Danielle Silva Pereira');
+INSERT INTO funcionario(nome) VALUES ('Isis Yume Okamoto');
+INSERT INTO funcionario(nome) VALUES ('Amanda Victoria Ribeiro');
